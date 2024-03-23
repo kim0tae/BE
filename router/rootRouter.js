fc27a1ba395c05397015f34540dc2469857c13de
Kim younftae <kimyt0402@gmail.com>
2024-03-23 14:21:31 +0900

    [UPDATE] 메인화면 이동 시 문구 출력 수정
    추후 메인페이지로 이동하는 화면 필요.

--- router/rootRouter.js
+++ router/rootRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 const rootRouter = express.Router();
 
-rootRouter.get('/', () => {
-  
+rootRouter.get('/', (req, res) => {
+  return res.send("hello world");
 });
 
 export default rootRouter;
