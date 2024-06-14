d6b5ccfa670f12e3abd228d38f990db90ee150f2
Kim youngtae <ytkim@infovine.co.kr>
2024-06-14 18:10:59 +0900

    [UPDATE] 코드정리

--- router/rootRouter.js
+++ router/rootRouter.js
@@ -2,13 +2,8 @@ import express from 'express';
 import cors from 'cors';
 //
 import { getJoin, postJoin, getLogin, postLogin } from '../controllers/userController.js';
-import { protectorMiddleware } from '../src/middlewares.js';
 
 const rootRouter = express.Router();
-
-rootRouter.get('/', (req, res) => {
-  return res.send({ page: 'home' });
-});
 
 rootRouter.route('/join').get(getJoin).post(postJoin);
 rootRouter.route('/login').get(getLogin).post(postLogin);
